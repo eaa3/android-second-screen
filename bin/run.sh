e85d7ee4297863f968f523decb6bb5c45b13695c
Ermano Arruda <ermano.arruda@gmail.com>
2017-12-16 18:36:09 +0000

    Update run.sh

--- bin/run.sh
+++ bin/run.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
 
-xrandr --fb 4160x1800 --output "DP-2" --scale 1.44x1.0 --panning 4160x1800+0+0/4160x1800+0+0
-xrandr --fb 4160x1800 --output "DP-2" --scale 1.44x1.0 --panning 2880x1800+0+0/4160x1800+0+0
+## Log and Run commands
+function run () {
+	echo "$1"
+	$1
+}
 
-# run x11vnc -clip 1280x720+2880+0 -nocursorshape -nocursorpos -xrandr -ncache 1 -nosel -viewonly -fixscreen \"V=2\" -noprimary -nosetclipboard -noclipboard -noxdamage -sb 0 -display :0
+
+# ./monitor.sh 1280x720 -left -v2
+run xrandr --fb 4160x1800 --output "DP-2" --scale 1.0x1.0 --panning 4160x1800+0+0/4160x1800+0+0
+sleep 5
+run xrandr --fb 4160x1800 --output "DP-2" --scale 1.0x1.0 --panning 2880x1800+0+0/4160x1800+0+0
+
+
 run x11vnc -clip 1280x720+2880+0
+
+xrandr --fb 2880x1800 --output "DP-2" --scale 1.0x1.0 --panning 2880x1800+0+0/2880x1800+0+0
+## Turn VirtualDisplay off
+# run "xrandr --output $virtual --off"
+# run "xrandr --delmode $virtual $mode"
+# run "xrandr --rmmode $mode"
+# run "xrandr -s 0"
